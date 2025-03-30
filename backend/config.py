91b7d27a99d4e4b51df3f5b71a5ffdaa5110c85f
yasminaba <115325777+yasminaba@users.noreply.github.com>
2025-03-29 23:35:26 -0400

    docs(config): add inline comments to clarify database connection settings

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,10 @@
 import os
 
-DB_USER = 'postgres'
-DB_PASSWORD = 'ilyasdata'  # Enter your password
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'ehotels'
-
-
+DB_USER = 'postgres' # Default is postgres, change if needed
+DB_PASSWORD = 'your_own_password'  # Enter your password
+DB_HOST = 'localhost' # Default is localhost, change if needed
+DB_PORT = '5432' # Default is 5432, change if needed
+DB_NAME = 'ehotels' # Enter your own database name (e.g., ehotels)
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
