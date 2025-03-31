173378217df7eba9be66b3034a81febaad6a0ef3
yasminaba <115325777+yasminaba@users.noreply.github.com>
2025-03-31 02:16:34 -0400

    fix(constraint): remove overly strict RoomProblems.ReportDate constraint

--- backend/database/constraints.sql
+++ backend/database/constraints.sql
@@ -33,7 +33,6 @@ ALTER TABLE Room
 -- RoomProblems Constraints
 ALTER TABLE RoomProblems
     ADD CONSTRAINT CHK_RoomProblem_NotNull CHECK (Problem IS NOT NULL AND LENGTH(TRIM(Problem)) > 0),
-    ADD CONSTRAINT CHK_RoomProblem_ReportDate CHECK (ReportDate <= CURRENT_DATE),
     ADD CONSTRAINT CHK_Resolved_Logic CHECK (Resolved IN (TRUE, FALSE));
 
 -- Customer Constraints
